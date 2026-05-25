ecf5085d6c1f356d494a60adda6ea8b0c0f9f00b
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-05-25 21:08:48 +0530

    Add mandatory field validation

---
+++ src/utils/validator.ts
@@ -0,0 +1,48 @@
+import validator from "validator"
+
+interface Registerbody {
+    firstName: string;
+    emailId: string;
+    password: string;
+}
+
+// Partial<RegisterBody> isliye use kiya hai kyunki req.body external/client data hai.
+// Hume 100% sure nahi hota ki client ne RegisterBody ke saare required fields bheje hain.
+// Example: client sirf firstName bhej sakta hai, emailId/password missing ho sakte hain.
+// Partial ka matlab hai data RegisterBody jaisa ho sakta hai, but uske fields optional ho sakte hain.
+// Iske baad hum manually mandatory fields check karte hain.
+// Agar koi important field missing hai, toh validation error throw kar dete hain.
+
+// Partial<Registerbody> ko aisa smjh sakte h
+// interface PartialRegisterbody {
+//     firstName?:string;
+//     emailId?:string;
+//     password?:string;
+// }
+
+const checkvalidator = (data: Partial<Registerbody>):void => {
+
+    const mandatoryField : Array<keyof Registerbody> = ["firstName", "emailId", "password"];
+
+    const check: boolean = mandatoryField.every((k) => Object.keys(data).includes(k));
+
+    if (!check) {
+        throw new Error("Missing Important Field");
+    }
+
+    if(typeof data.firstName !== "string" || !validator.isLength(data.firstName,{min:3,max:20})){
+        throw new Error("Firstname is not valid");
+    }
+
+    if(typeof data.password !== "string" || validator.isStrongPassword(data.password,{
+        minLength:8,
+        minLowercase:1,
+        minUppercase:1,
+        minNumbers:1,
+        minSymbols:2,
+    })){
+        throw new Error ("Password must contain UpperCase , LowerCase , Number and Special Symbol");
+    }
+}
+
+export default checkvalidator;
